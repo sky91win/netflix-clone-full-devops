5e211beffb679e175eac4f5ce2b749ef9a24b1cf
sky91win <118115504+sky91win@users.noreply.github.com>
2025-12-27 22:18:45 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,16 +1,26 @@
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.3"
+
   cluster_name    = var.cluster_name
   cluster_version = "1.29"
-  vpc_id          = module.vpc.vpc_id
-  subnet_ids      = module.vpc.private_subnets
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
 
   eks_managed_node_groups = {
     default = {
+      desired_size = 1
       min_size     = 1
-      max_size     = 3
-      desired_size = 2
-      instance_types = ["t3.medium"]
+      max_size     = 1
+
+      instance_types = ["m7i-flex.large"]
+
+      disk_size = 30   # 👈 30 GB EBS per worker node
     }
   }
 }
+
